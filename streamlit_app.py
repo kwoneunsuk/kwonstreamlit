7d7cf1c0eeca76417528bca5f4b6f01ced59918f
kwoneunsuk <lemon91212@gmail.com>
2024-07-25 18:21:00 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,5 +15,5 @@ st.text('과정4. 스트림릿에서 앱 코드 배포')
 st.subheader('제곱 계산기')
 st.write('숫자를 입력하면 제곱값을 계산해드려요') 
 number=st.number_input('숫자를 입력하세요.', min_value=0, value=0) 
-squared_number=number**2 
-st.write(f'{number]의 제곱은 {squared_number]입니다.')
+squared_number = number ** 2 
+st.write(f'{number}의 제곱은 {squared_number}입니다.')
