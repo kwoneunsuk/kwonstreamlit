46d4aa001b81f5fd0684229ba9ae388099bcb12c
kwoneunsuk <lemon91212@gmail.com>
2024-07-25 18:19:40 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,10 @@ st.text('과정1. 깃허브(github.com) 계정 생성')
 st.text('과정2. 스트림릿(steamlit.io) 계정 생성 - 이 때 반드시 깃허브 계정과 연동')
 st.text('과정3. 깃허브에서 앱 코드(main.py) 작성')
 st.text('과정4. 스트림릿에서 앱 코드 배포')
+
+
+st.subheader('제곱 계산기')
+st.write('숫자를 입력하면 제곱값을 계산해드려요') 
+number=st.number_input('숫자를 입력하세요.', min_value=0, value=0) 
+squared_number=number**2 
+st.write(f'{number]의 제곱은 {squared_number]입니다.')
